8c3e02effac7c6eef7ac62c412bd0ea8e7af0aa7
Nadia <joystate@me.com>
2018-05-07 14:51:05 -0400

    fix memory leak

--- BackgroundImageFetch.swift
+++ BackgroundImageFetch.swift
@@ -11,6 +11,8 @@ import UIKit
 public class BackgroundImageFetch {
 
     private var urls: [URL] = []
+    private var manager: KingfisherManager = KingfisherManager.shared
+    private var session: URLSession = URLSession.shared
 
     public convenience init(urls: [URL]) {
         self.init()
@@ -19,13 +21,12 @@ public class BackgroundImageFetch {
 
     public func fetch() {
         DispatchQueue.global(qos: .background).async {
-            let manager = KingfisherManager.shared
             for imageUrl in self.urls {
-                if manager.cache.imageCachedType(forKey: imageUrl.absoluteString) == .none {
-                    URLSession.shared.dataTask(with: imageUrl, completionHandler: { (data, _, _) in
+                if self.manager.cache.imageCachedType(forKey: imageUrl.absoluteString) == .none {
+                    self.session.dataTask(with: imageUrl, completionHandler: { (data, _, _) in
                         if let data = data, let image = UIImage(data: data) {
-                            if manager.cache.imageCachedType(forKey: imageUrl.absoluteString) == .none {
-                                manager.cache.store(image, forKey: imageUrl.absoluteString)
+                            if self.manager.cache.imageCachedType(forKey: imageUrl.absoluteString) == .none {
+                                self.manager.cache.store(image, forKey: imageUrl.absoluteString)
                             }
                         }
                     }).resume()
@@ -33,6 +34,4 @@ public class BackgroundImageFetch {
             }
         }
     }
-
-    
 }
